2de1563c0dae7e17917ac955cbc047a1d3f86bd8
Mateus <matt_ufo2@hotmail.com>
2025-11-12 11:49:08 -0300

    Criado migration para polular as tabelas de categoria e produtos

---
+++ APICatalogo/Migrations/20251112135413_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations // Namespace para migrações do Entity Framework
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration // Migração para popular a tabela de categorias com dados iniciais
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb) // Popula a tabela de categorias com dados iniciais
+        {
+            mb.Sql("INSERT INTO Category (Name, ImageUrl) VALUES('Bebidas', 'bebidas.png')");
+
+            mb.Sql("INSERT INTO Category (Name, ImageUrl) VALUES('Lanches', 'lanches.png')");
+
+            mb.Sql("INSERT INTO Category (Name, ImageUrl) VALUES('Sobremesas', 'sobremesas.png')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb) // Remove os dados inseridos na tabela de categorias
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20251112141926_PopularProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products (Name, Description, Price, ImageUrl, Stock, RegisterDate, CategoryId) " +
+                   "VALUES('Coca-Cola', 'Refrigerante de cola 350ml', 5.00, 'coca_cola.png', 52, NOW(), 1)");
+
+            mb.Sql("INSERT INTO Products (Name, Description, Price, ImageUrl, Stock, RegisterDate, CategoryId) " +
+                   "VALUES('Sanduiche Frango', 'Sanduiche de Frango 200g', 9.00, 'sanduiche_frango.png', 30, NOW(), 2)");
+
+            mb.Sql("INSERT INTO Products (Name, Description, Price, ImageUrl, Stock, RegisterDate, CategoryId) " +
+                   "VALUES('Pudim', 'Pudim gelado 100g', 7.00, 'pudim.png', 12, NOW(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
